56aff300b8ecb79d800ea660c47374426d8f2245
Opilane TTHK <andero.magi.420+schoolgit@gmail.com>
2024-09-16 10:58:01 +0300

    Student model

--- ContosoUni/Models/Student.cs
+++ ContosoUni/Models/Student.cs
@@ -1,12 +1,25 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices;
 
 namespace ContosoUni.Models
 {
     public class Student
     {
         public int ID { get; set; }
+
+        [StringLength(maximumLength: 50)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [StringLength(maximumLength: 50)]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment>? Enrollments { get; set; }
